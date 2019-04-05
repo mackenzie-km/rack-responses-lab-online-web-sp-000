e374d157d6844ce3bca72a73c76e89d95f831c06
Mackenzie Moore <macmoore@ucdavis.edu>
2019-04-05 18:00:30 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -2,7 +2,7 @@ class Application
   def call(env)
     resp = Rack::Response.new
     now = Time.now.hour.to_i
-    if now < 12 then resp.write "Morning" else resp.write "Afternoon" end
+    if now < 12 then resp.write "Morning" else resp.write "Afternoon #{now}" end
     resp.finish
   end
 end
