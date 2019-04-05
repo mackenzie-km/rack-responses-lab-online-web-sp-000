c817ad6b2c7542b46d5d8a2e4e305ba7921b3eb7
Mackenzie Moore <macmoore@ucdavis.edu>
2019-04-05 17:57:25 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,8 +1,8 @@
 class Application
   def call(env)
     resp = Rack::Response.new
-    resp.write "Good morning"
-    resp.write "Good afternoon"
+    now = Time.now.hour.to_i
+    now < 12 ? resp.write "Good morning" : resp.write "Good afternoon"
     resp.finish
   end
 end
